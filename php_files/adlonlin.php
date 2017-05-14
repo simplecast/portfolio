9acdd1e721a540095bf23b0fb682c665946a7d78
Kishon Diaz <simplecastic@gmail,com>
2017-05-14 01:06:09 -0400

    working on php

--- php_files/adlonlin.php
+++ php_files/adlonlin.php
@@ -4,23 +4,16 @@
 
     }
   }
-
+  $logg;
   foreach($db->query('SELECT `Logged_on` FROM `admin` WHERE 1') as $h){
-    echo $h['Logged_on'];
+    $logg = $h['Logged_on'];
   };
-  
-  //echo $db->exec('SELECT `Adid` FROM `admin` WHERE 1');
-
-
-  
- // print( $db->exec('SELECT `Adid` FROM `admin` WHERE `Adid` = 0'));
-  /*echo $logg;
-    if($logg === true){
-      $logg = "here";
-    }else{
-      $logg = "there";
-    };
-*/
+  echo $logg;
+  if($logg === "1"){
+    $logg = "here";
+  }else{
+    $logg = "there";
+  }
 ?>
 <script type="text/javascript">
   adlog = "<?php echo $logg?>";
