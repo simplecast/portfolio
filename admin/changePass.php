3b2ad70f7269ef86c9c975f35eb188a67a4acc38
Kishon Diaz <simplecastic@gmail,com>
2017-05-17 04:15:36 -0400

    Working on change Password php page

--- admin/changePass.php
+++ admin/changePass.php
@@ -1,9 +1,49 @@
-<?php
-  include('../php_files/dbconnection.php');
-  $passw ="";
-  
-  foreach($db->query('SELECT * FROM `users` WHERE 1') as $h){
-    echo $h['Password'];
-  };
+<!doctype html>
+<html>
+  <head>
+    
+    <title>Change Password</title>
+    <?php
+      include('../php_files/dbconnection.php');
+      $passw ="";
+      $newpass="";
+      $conpass = "";
+      foreach($db->query('SELECT * FROM `users` WHERE 1') as $h){
+        echo $h['Password'];
+      };
+      for($i=0;$i < 0; $i++){
+        header("Location: ./changePass.php");
+      }
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(isset($_POST['nPsub'])){
+          if(!empty($_POST['newpass'])){
+            
+          }
+          if(!empty($_POST['confirmpass'])){
+            
+          }
+          if($_POST['newpass'] !== $_POST['confirmpass']){
+            echo "Not match";
+          }else{
+            
+          }
+          
+          
+        }
+      }
 
-?>
+    ?>
+  </head>
+  <body>
+    <form>
+      <label>New Password</label>
+      <input name="newpass" type="text">
+      <label>Confirm Password</label>
+      <input name="confirmpass" type="text">
+      <input name="nPsub" type="submit" value="Submit">
+      <input type="reset" value="Reset">
+    
+    </form>
+  
+  </body>
+</html>
--- admin/login.php
+++ admin/login.php
@@ -5,40 +5,73 @@
     <?php
       $adusers="";
       $adpass="";
+      $usern = " ";
+      $passw = " ";
       include('../php_files/dbconnection.php');
       foreach($db->query("SELECT * FROM `users` WHERE 1 ") as $h){
          $adusers = $h['Username'];
          $adpass = $h['Password'];
       }
+      echo $adpass;
       $logprocess = './login.php';
       $isempty = [true,true,true,true];
+      $isempty1 = true;
+      $isempty2 = true;
+      $isempty3 = true;
+      $isempty4 = true;
       function validation(){
         global $isempty;
-        global $aduser;
+        $isempty1 = true;
+        global $adusers;
+        global $adpass;
+        global $usern;
+        global $passw;
+        
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          echo "appp";
           if(isset($_POST['logsub'])){
-            if(!empty($_POST['username'])){
+            $usern = $_POST['username'];
+            $passw = $_POST['password'];
+            if(!empty($usern)){
               $isempty[0] = false;
-            }else 
-              if($_POST['username'] === $aduser){
-              $isempty[1] = false;
-            }else
-            if(!empty($_POST['password'])){
-              $isempty[2] = false;
-            }else
-            if($_POST['password'] === $adpass){
-                $isempty[3] = false;
+              $isempty1 = false;
+              echo "Asd";
             }
             
-            
-            if($isempty[0] === false && $isempty[1] === false &&
-               $isempty[2] === false && $isempty[3] ===false){
-             
+            if($usern === $adusers){
+              $isempty[1] = false;
+              echo "Asdd";
+            }else{
+              $isempty[1] = true;
+            }
+            if(!empty($passw)){
+              $isempty[2] = false;
+              echo "dfaaass";
+            }
+            if($passw === $adpass){
+                $isempty[3] = false;
+              echo "shhd";
+            }
+            if($isempty[0]==false){
+              echo "heres";
+            }
+            if($isempty[0]=== false && $isempty[1]==false &&
+               $isempty[2] === false && $isempty[3] === false){
+              echo "herse";
+              if($adpass === "pa11word"){
+                $usern = "";
+                $_POST['username'] = "";
+                header("Location: ./changePredirect.php");
+              }
+             $_POST['username'] = "";
               return true;
               
             }
-            
-            return false;
+            $usern = "";
+            $passw = "";
+            $_POST['username'] = "";
+            $_POST['password'] = "";
+            echo false;
           }
           
         }
@@ -47,15 +80,16 @@
     
   </head>
   <body>
-    <form action="<?php echo $logprocess;?>" onsubmit="<?php validation(); ?>" method="post">
+    <?php echo validation();?>
+    <form action="<?php echo $logprocess;?>" onsubmit="php:return  <?php validation(); ?>" method="post">
       <label for="username">Username</label>
-      <input name="username" id="username" type="text">
+      <input name="username" id="username" type="text" autocomplete="off" value="<?php echo $usern; ?>">
       <br>
       <label for="password">Password</label>
-      <input name="password" id="password" type="password"><br>
+      <input name="password" id="password" type="password" <?php echo $passw;?>><br>
       <input name="logsub" type="submit" value="Log In">
       <input type="reset" value="Reset">
     </form>
-<!--    <?php/// echo validation();?>-->
+    
   </body>
 </html>
