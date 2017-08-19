b5c8d8eddf716fe53befa91db2b16c3726cb29e0
simplecast <simplecastic@gmail.com>
2017-08-19 02:52:21 -0400

    adding in fluid grid

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Simple Sidebar - Start Bootstrap Template</title>
+    <title>Kishon Resume</title>
 
     
     <!-- Bootstrap core CSS -->
@@ -19,6 +19,10 @@
     <link href="css/simple-sidebar.css" rel="stylesheet">
     
     <link href="css/full-slider.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="fluid-gallery.css">
+
     <link href="css/maincss.css" rel="stylesheet" >
     
     
@@ -75,106 +79,146 @@
                       <div class="bar2"></div>
                       <div class="bar3"></div>
                     </div>
-                    Kishon Diaz
-                </a>
-                
-                <a class="navbar-brand btn btn-secondary openbanner" href="" id="">
-                Return Text
-                    <img src="assets/images/bootstrap.png" width="30" height="30" class="" alt="">
-                    
+                    <span id="breadcrumbs">Home</span>
                 </a>
             </nav>
-            <div>
-              <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-              <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+            <div class="page-header modal-header header">
+                <h1 class="col-md-12" id="headertext">Salutations my name is Kishon Diaz </h1>   
             </div>
-            <div class="jumbotron projections">
+            <div class="context">
+                <div class="span mx-auto">I am passionate about programming and expanding my knowledge in the field of technology. I have strong work ethics, dependable and professional. Once I am given the opportunity to be a programmer for your company it will open the door to develop new technologies and innovative ideas.<br>
+                </div>
+            <div class="projections">
               <div class="row">
-                <!-- <div class="mx-auto">
+                <div class="col-md-12 homeimg">
                   <img src="./assets/images/kishon_image.png" alt="kishon" class="rounded mx-auto d-block" width="" height="">
-                </div> -->
-                <div class="mx-auto">
-                  
-                
-                <div class="mx-auto">
-                <div id="carousel-items" class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-2 carousel slide gal" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-items" data-slide-to="0" class="active item">
-                  <img src="http://placehold.it/600x400" alt="" class="imgItem">
-                </li>
-                <li data-target="#carousel-items" data-slide-to="1" class="item">
-                  <img src="http://placehold.it/600x400" alt="" class="imgItem">
-                </li>
-                <li data-target="#carousel-items" data-slide-to="2" class="item">
-                  <img src="http://placehold.it/600x400" alt="" class="imgItem">
-                </li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-                <!-- Slide One - Set the background image for this slide in the line below -->
-                <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h3>First Slide</h3>
-                        <p>This is a description for the first slide.</p>
-                    </div>
                 </div>
-                <!-- Slide Two - Set the background image for this slide in the line below -->
-                <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h3>Second Slide</h3>
-                        <p>This is a description for the second slide.</p>
+               <div class="mx-auto">     
+                <div class="mx-auto">
+                 <div id="carousel-items" class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-2 carousel slide gal" data-ride="carousel">
+                   <ol class="carousel-indicators">
+                    <li data-target="#carousel-items" data-slide-to="0" class="active item">
+                     <img src="http://placehold.it/600x400" alt="" class="imgItem">
+                    </li>
+                    <li data-target="#carousel-items" data-slide-to="1" class="item">
+                      <img src="http://placehold.it/600x400" alt="" class="imgItem">
+                    </li>
+                    <li data-target="#carousel-items" data-slide-to="2" class="item">
+                      <img src="http://placehold.it/600x400" alt="" class="imgItem">
+                    </li>
+                  </ol>
+                <div class="carousel-inner col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-2" role="listbox">
+                    <!-- Slide One - Set the background image for this slide in the line below -->
+                    <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>First Slide</h3>
+                            <p>This is a description for the first slide.</p>
+                        </div>
                     </div>
-                </div>
-                <!-- Slide Three - Set the background image for this slide in the line below -->
-                <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+                    <!-- Slide Two - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Second Slide</h3>
+                            <p>This is a description for the second slide.</p>
+                        </div>
+                    </div>
+                    <!-- Slide Three - Set the background image for this slide in the line below -->
+                    <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
                     <div class="carousel-caption d-none d-md-block">
                         <h3>Third Slide</h3>
                         <p>This is a description for the third slide.</p>
                     </div>
+                 </div>
                 </div>
+                  <a class="carousel-control-prev" href="#carousel-items" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carousel-items" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                   </a>
+                </div>
+                </div>
+                <div class="container gallery-container">
+
+    <h1>Bootstrap 3 Gallery</h1>
+
+    <p class="page-description text-center">Fluid Layout With Overlay Effect</p>
+    
+    <div class="tz-gallery">
+
+        <div class="row">
+
+            <div class="col-sm-12 col-md-4">
+                <a class="lightbox" href="./assets/images/bridge.jpg">
+                    <img src="./assets/images/bridge.jpg" alt="Bridge">
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carousel-items" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel-items" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="./assets/images/park.jpg">
+                    <img src="./assets/images/park.jpg" alt="Park">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="./assets/images/tunnel.jpg">
+                    <img src="./assets/images/tunnel.jpg" alt="Tunnel">
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-8">
+                <a class="lightbox" href="./assets/images/traffic.jpg">
+                    <img src="./assets/images/traffic.jpg" alt="Traffic">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="./assets/images/coast.jpg">
+                    <img src="./assets/images/coast.jpg" alt="Coast">
+                </a>
+            </div> 
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="./assets/images/rails.jpg">
+                    <img src="./assets/images/rails.jpg" alt="Rails">
+                </a>
+            </div>
+
         </div>
-                </div>
-              </div>
-</div>   
+
+    </div>
+
+</div>
+               </div>
+              </div>   
             </div>
           </div>
         </div>
       </div>
     </div>
     <!-- /#page-content-wrapper -->
-  </div>
+
   <!-- /#wrapper -->
   
   <!-- Bootstrap core JavaScript -->
   <script src="js/jquery.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
 
   <!-- Menu Toggle Script -->
   <script>
-   var menucontainer = $('.menucontainer');
-   menucontainer.on('click',function(){
-     $this.toggleClass('change');
-   })
-  $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-      menucontainer.toggleClass("change");
-      
-  });
-
-  
- 
+    var menucontainer = $('.menucontainer');
+    menucontainer.on('click',function(){
+      $this.toggleClass('change');
+    })
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        menucontainer.toggleClass("change");
+    });
   </script>
-
+  <script>
+    baguetteBox.run('.tz-gallery');
+  </script>
+  <script type="text/javascript" src="./js/mainjs.js"></script>
 </body>
 
 </html>
